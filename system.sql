ff43bb9c2e3cf6f058de1a80033b187a180afaea
patryksz <patrykszydlik3@gmail.com>
2021-05-08 16:06:44 +0200

    mamy baze, jest megamzyczo !

--- system.sql
+++ system.sql
@@ -21,80 +21,128 @@ create sequence SQ_Konkurencje start with 1 increment by 1;
 create sequence SQ_Stanowiska start with 1 increment by 1;
 create sequence SQ_Sesje start with 1 increment by 1;
 create sequence SQ_Sparingi start with 1 increment by 1;
-create sequence SQ_Przejazdy_czasowy start with 1 increment by 1;
+create sequence SQ_Przejazdy_czasowe start with 1 increment by 1;
 create sequence SQ_Sedziowie start with 1 increment by 1;
 
 ---
 --- Czyszczenie tabel
 ---
 
-drop table TB_Zawodnicy;
-drop table TB_Roboty;
-drop table TB_Konkurencja;
-drop table TB_Stanowiska;
-drop table TB_Sesje;
 drop table TB_Sparingi;
-drop table TB_Przejazd_czasowy
+drop table TB_Przejazd_czasowy;
+drop table TB_Roboty;
+drop table TB_Sesje;
+drop table TB_Konkurencje;
+drop table TB_Stanowiska;
 drop table TB_Sedziowie;
-
+drop table TB_Zawodnicy;
 ---
 --- Tworzenie tabel
 ---
 
 create table TB_Zawodnicy (
     ID_Zawodnika number primary key,
-    Imie varchar2 not null,
-    Nazwisko varchar2 not null,
-    Adres varchar2 not null,
-    Uczelnia varchar2,
-    Aktywny boolean
+    Imie varchar2(32) not null,
+    Nazwisko varchar2(32) not null,
+    Mail varchar2(32) not null,
+    Adres varchar2(32) not null,
+    Uczelnia varchar2(32),
+    Aktywny number(1) default 0 not null
+);
+
+
+create table TB_Sedziowie (
+    ID_Sedziego number primary key,
+    Imie varchar(32) not null,
+    Nazwisko varchar(32) not null
+);
+
+create table TB_Konkurencje (
+    ID_Konkurencji number primary key,
+    Nazwa varchar2(32) not null,
+    Typ_oceny varchar2(32) not null,
+    Godzina_startu date not null,
+    Zakonczona number(1) default 0 not null
 );
 
 create table TB_Roboty (
     ID_Robota number primary key,
-    ID_Zawodnika number foreign key,
-    ID_Konkurencji number foreign key,
-    Nazwa_robota varchar2 not null,
-);
-
-create table TB_Konkurencja (
-    ID_Konkurencji number primary key,
-    Nazwa varchar2 not null,
-    Typ_oceny varchar2 not null,
-    Godzina_startu date not null,
-    Zakończona boolean
+    ID_Zawodnika number,
+    ID_Konkurencji number,
+    Nazwa_robota varchar2(32) not null,
+    CONSTRAINT FK_Zawodnik_Robot FOREIGN KEY (ID_Zawodnika)
+    REFERENCES TB_Zawodnicy (ID_Zawodnika),
+    CONSTRAINT FK_Konkurencja_Robot FOREIGN KEY (ID_Konkurencji)
+    REFERENCES TB_Konkurencje (ID_Konkurencji)
 );
 
 create table TB_Stanowiska (
     ID_Stanowiska number primary key,
-    Stan boolean
+    Stan number(1) default 0 not null
 );
 
 create table TB_Sesje (
     ID_Sesji number primary key,
-    ID_Konkurencji number foreign key,
-    ID_Sedziego number foreign key,
-    ID_Stanowiska number foreign key,
-    Godzina_startu date not null
+    ID_Konkurencji number,
+    ID_Sedziego number,
+    ID_Stanowiska number,
+    Godzina_startu date not null,
+    CONSTRAINT FK_Konkurencja_Sesja FOREIGN KEY (ID_Konkurencji)
+    REFERENCES TB_Konkurencje (ID_Konkurencji),
+    CONSTRAINT FK_Sedzia_Sesja FOREIGN KEY (ID_Sedziego)
+    REFERENCES TB_Sedziowie (ID_Sedziego),
+    CONSTRAINT FK_Stanowisko_Sesja FOREIGN KEY (ID_Stanowiska)
+    REFERENCES TB_Stanowiska (ID_Stanowiska)
 );
 
 create table TB_Sparingi (
     ID_Sparingu number primary key,
-    ID_Robota number foreign key,
-    ID_Sesji number foreign key,
-    Punkty number not null
+    ID_Robota number,
+    ID_Sesji number,
+    Punkty number not null,
+    CONSTRAINT FK_Robot_Sparing FOREIGN KEY (ID_Robota)
+    REFERENCES TB_Roboty (ID_Robota),
+    CONSTRAINT FK_Sesja_Sparing FOREIGN KEY (ID_Sesji)
+    REFERENCES TB_Sesje (ID_Sesji)
 );
 
 create table TB_Przejazd_czasowy (
     ID_Przejazdu number primary key,
-    ID_Robota number foreign key,
-    ID_Sesji number foreign key,
+    ID_Robota number,
+    ID_Sesji number,
     Czas number not null,
+    CONSTRAINT FK_Robot_Przejazd_Czasowy FOREIGN KEY (ID_Robota)
+    REFERENCES TB_Roboty (ID_Robota),
+    CONSTRAINT FK_Sesja_Przejazd_Czasowy FOREIGN KEY (ID_Sesji)
+    REFERENCES TB_Sesje (ID_Sesji)
 );
 
-create table TB_Sedziowie (
-    ID_Sedziego number primary key,
-    Imie varchar2 not null,
-    Nazwisko varchar2 not null,
-);
+
+---
+--- Wypelnianie tabeli
+---
+
+insert into TB_Zawodnicy
+values ( SQ_Zawodnicy.nextval ,'Gregory',  'Mzyku', 'patryk.szydlik@tlen.pl', 'Litomska Wroclaw', 'PWR', 0);
+
+insert into TB_Sedziowie
+values (SQ_Sedziowie.nextval,'Grzegorz','Mzyk');
+
+insert into TB_Konkurencje
+values (SQ_Konkurencje.nextval,'LineFollower', 'Czasowy', TIMESTAMP '2021-05-08 18:30:00', 0);
+
+insert into TB_Roboty
+values ( SQ_Roboty.nextval, SQ_Zawodnicy.currval, SQ_Konkurencje.currval, 'MegaZord' );
+
+insert into TB_Stanowiska
+values (SQ_Stanowiska.nextval, 0);
+
+insert into TB_Sesje
+values (SQ_Sesje.nextval, SQ_Konkurencje.currval, SQ_Sedziowie.currval, SQ_Stanowiska.currval, TIMESTAMP '2021-05-08 18:30:00');
+
+insert into TB_Sparingi
+values (SQ_Sparingi.nextval, SQ_Roboty.currval, SQ_Sesje.currval, 0 );
+ 
+insert into TB_Przejazd_czasowy
+values (SQ_Przejazdy_czasowe.nextval, SQ_Roboty.currval, SQ_Sesje.currval, 0);
 
