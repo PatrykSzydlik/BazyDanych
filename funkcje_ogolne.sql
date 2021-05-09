7b66e1bb6759f9f0ddc88f1dba3b6d1e8f55485d
unknown <wiktorbajor1@gmail.com>
2021-05-09 21:25:57 +0200

    Dodane funkcje ogolne

--- funkcje_ogolne.sql
+++ funkcje_ogolne.sql
@@ -4,18 +4,56 @@
 
 create or replace procedure wyswietl_ranking_punktowy(
     id_konkurencji_p number,
-    ilosc number
+    ilosc_robotw number
 )
 as
+    id_sesji_p number;
+    i number;
+    nazwa_r varchar2(40);
 begin
+    i := 1;
+    select ID_Sesji
+    into id_sesji_p
+    from TB_Sesje
+    where ID_Konkurencji = id_konkurencji_p;
+    for rec in (select ID_Robota, Punkty
+                from TB_Sparingi
+                where ID_sesji = id_sesji_p order by Punkty desc)
+    loop
+        exit when i = ilosc_robotw;
+        select Nazwa_robota into nazwa_r from TB_Roboty where ID_Robota = rec.ID_Robota;
+        dbms_output.put_line(rec.ID_Robota ||' ' ||
+                             rec.Punkty ||' ');
+        i := i + 1;
+    end loop;
+    
 end;
 /
 
 create or replace procedure wyswietl_ranking_czasowy(
     id_konkurencji_p number,
-    ilosc number
+    ilosc_robotw number
 )
 as
+    id_sesji_p number;
+    i number;
+    nazwa_r varchar2(40);
 begin
+    i := 1;
+    select ID_Sesji
+    into id_sesji_p
+    from TB_Sesje
+    where ID_Konkurencji = id_konkurencji_p;
+    for rec in (select ID_Robota, Czas
+                from TB_Przejazd_czasowy
+                where ID_sesji = id_sesji_p order by Czas)
+    loop
+        exit when i = ilosc_robotw;
+        select Nazwa_robota into nazwa_r from TB_Roboty where ID_Robota = rec.ID_Robota;
+        dbms_output.put_line(rec.ID_Robota ||' ' ||
+                             rec.Czas ||' ');
+        i := i + 1;
+    end loop;
+    
 end;
 /
