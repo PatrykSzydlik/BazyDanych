42f8542cc5b2006ffcf00dab7cb1606f2bb110f8
unknown <wiktorbajor1@gmail.com>
2021-05-09 13:53:25 +0200

    Funkcje wyswietlanie dziala.

--- system.sql
+++ system.sql
@@ -181,19 +181,33 @@ begin
 end;
 /
 
+
+---
+--- Funkcja dziala, trzeba w sql developer wejsc w View , Dbms output i ustawic enable
+---
 create or replace procedure wyswietl_zawodnika(
     Imie_zawodnika varchar2,
     Nazwisko_zawodnika varchar2,
     Mail_zawodnika varchar2
 )
 as
-    id out number;
-    adres out varchar2(32);
-    uczelnia out varchar2(32);
-    aktywny out number;
+    id number;
+    adres varchar2(32);
+    uczelnia  varchar2(32);
+    aktywny  number;
 begin
-    select ID_Zawodnika, Adres, Uczelnia, Aktywny  into id, adres, uczelnia, aktywny from TB_Zawodnicy where Imie = Imie_zawodnika and Nazwisko = Nazwisko_zawodnika and Mail = Mail_zawodnika; 
-    dbms_output.put_line(id || ' ' || adres || ' - ' || uczelnia);
+    select ID_Zawodnika, Adres, Uczelnia, Aktywny
+    into id, adres, uczelnia, aktywny
+    from TB_Zawodnicy
+    where Imie = Imie_zawodnika and Nazwisko = Nazwisko_zawodnika and Mail = Mail_zawodnika;
+    dbms_output.put_line(id ||' '||
+                        Imie_zawodnika ||' '||
+                        Nazwisko_zawodnika ||' '||
+                        Mail_zawodnika ||' '|| 
+                        adres ||' '|| 
+                        uczelnia ||' '|| 
+                        aktywny || ' ' 
+                        );
 end;
 /
 
